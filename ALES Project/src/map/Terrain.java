fec68f34437e3024963497e9c4e0e0937584ec13
bannigeri17 <bannigeri17@gmail.com>
2016-08-03 10:47:45 -0700

    Expand window of prey for hunter cells

--- ALES Project/src/map/Terrain.java
+++ ALES Project/src/map/Terrain.java
@@ -339,22 +339,24 @@ public class Terrain {
      */
     public int hunt(List<Cell> hunters) {
         int nutrientsGained = 0;
-        for (int i = hunters.size()-1; i>=0; i--) {
+        for (int i = hunters.size() - 1; i >= 0; i--) {
             Cell ce = hunters.get(i);
             Creature cr = ce.getCreature();
             int x = ce.getX() + cr.getPosX();
             int y = ce.getY() + cr.getPosY();
-            for (int k = 0; k < 4; k++) {
-                int newX = x + Mapping.ADX4[k];
-                int newY = y + Mapping.ADY4[k];
-                Cell prey = cellAtAbsPos(newX, newY);
-                boolean inBounds = newX < width && newY < height && newX >= 0 && newY >= 0;
-                if (inBounds && prey != null) {
-                    nutrientsGained += (int) (hunterYield * prey.getMaxStore());
-                    prey.damage();
-                    if (prey.getHp() <= 0) {
-                        Creature victim = prey.getCreature();
-                        victim.deleteCell(prey);
+            for (int count = -2; count <= 2; count++){
+                int newX = x + count;
+                for (int j = -2; j <= 2; j++) {
+                    int newY = y + j;
+                    Cell prey = cellAtAbsPos(newX, newY);
+                    boolean inBounds = newX < width && newY < height && newX >= 0 && newY >= 0;
+                    if (inBounds && prey != null) {
+                        nutrientsGained += (int) (hunterYield * prey.getMaxStore());
+                        prey.damage();
+                        if (prey.getHp() <= 0) {
+                            Creature victim = prey.getCreature();
+                            victim.deleteCell(prey);
+                        }
                     }
                 }
             }
