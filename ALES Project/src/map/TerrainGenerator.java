e046ed0a510b2cccb62cf98628b4cc1a600bc47c
bannigeri17 <bannigeri17@gmail.com>
2016-08-04 11:53:41 -0700

    Add getParent method to find creature parents in sim

--- ALES Project/src/map/TerrainGenerator.java
+++ ALES Project/src/map/TerrainGenerator.java
@@ -14,34 +14,36 @@ import util.Noise;
  * @author Kosmic
  */
 public class TerrainGenerator {
-    
+
     public static double foodSpawnRate = 0.005;
-    
-    public static Terrain generate(int size, double seed){
-        
+
+    public static Terrain generate(int size, double seed) {
+
         int[][] map = new int[size][size];
         Noise n = new Noise(1.0);
         n.seed = seed;
-        
+        double[][] probMap = new double[size][size];
+
         for (int i = 0; i < size; i++) {
-            
+
             for (int j = 0; j < size; j++) {
-                
+
                 double h = n.multi(i, j, 100, 0.01);
+                probMap[i][j] = h;
                 
-                if(Math.abs(h) > 0.8){
-                    
+                if (Math.abs(h) > 0.8) {
+
                     map[i][j] = h > 0 ? 2 : 3;
-                }else{
-                    
-                    if((Math.abs((new Random()).nextDouble() * h)) < foodSpawnRate){
-                        
+                } else {
+
+                    if ((Math.abs((new Random()).nextDouble() * h)) < foodSpawnRate) {
+
                         map[i][j] = 1;
                     }
                 }
             }
         }
-        
-        return new Terrain(map, new ArrayList());
+
+        return new Terrain(map, new ArrayList(), probMap);
     }
 }
