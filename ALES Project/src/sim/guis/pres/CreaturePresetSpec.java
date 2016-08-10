2ea587d87520cfcf65254f3fb48a636105f3b547
Cruz Barnum <cruzbarnum@gmail.com>
2016-08-10 15:04:25 -0700

    FINISH TERRAIN PRESET FOR TEH COMMAND FIELDS.

--- ALES Project/src/sim/guis/pres/CreaturePresetSpec.java
+++ ALES Project/src/sim/guis/pres/CreaturePresetSpec.java
@@ -6,15 +6,18 @@
 package sim.guis.pres;
 
 import gui.GUIController;
+import static gui.GUIController.FONT;
 import static gui.TypingManager.typing;
 import gui.components.GUIButton;
 import gui.components.GUICommandField;
 import gui.components.GUILabel;
 import gui.components.GUIPanel;
 import gui.types.ComponentInputGUI;
+import gui.types.GUIComponent;
 import gui.types.GUIInputComponent;
 import static java.lang.Double.parseDouble;
 import static java.lang.Integer.parseInt;
+import java.util.List;
 import org.newdawn.slick.Color;
 import sim.guis.NewPreset;
 import util.Color4;
@@ -35,28 +38,33 @@ public class CreaturePresetSpec extends ComponentInputGUI {
     public CreaturePresetSpec(String n, NewPreset parent) {
         super(n);
         this.parent = parent;
-                for (int i = 0; i < 5; i++) {
-            components.add(new GUIPanel("top1." + i, nextPlace(parent.getStartPos(), 3, -i), BUTTON_SIZE.multiply(new Vec2(1, 1)), getColor(0).multiply(0.8 - 0.1 * i)));
+        for (int i = 0; i < 4; i++) {
+            components.add(new GUIPanel("top1." + i, nextPlace(parent.getStartPos(), 3, -i), BUTTON_SIZE.multiply(new Vec2(2 - (i == 3 ? 1 : 0), 1)), getColor(0).multiply(0.8 - 0.1 * i)));
         }
         inputs.add(new GUIButton("cancelBtn", this, nextPlace(parent.getStartPos(), 3, 1), BUTTON_SIZE, "Cancel", Color.white));
         components.add(new GUIPanel("cancelPanel", nextPlace(parent.getStartPos(), 3, 1), BUTTON_SIZE, Color4.RED.multiply(0.8)));
-        
-        inputs.add(new GUIButton("eCostSetVisible", this, nextPlace(parent.getStartPos(), 3, -4), BUTTON_SIZE, "Energy Costs", Color.white));
+
+        inputs.add(new GUIButton("eCostSetVisible", this, nextPlace(parent.getStartPos(), 3, -3), BUTTON_SIZE, "Energy Costs", Color.white));
         //ENERGY COSTS
         eCostMenu = new EnergyCostMenu("eCostMenu", this);
         GUIController.add(eCostMenu);
         // </energyCosts>
         //THRESHOLDS
-        components.add(new GUILabel("reproThreshLabel", nextPlace(parent.getStartPos(), 3, -3), BUTTON_SIZE, "Mate Threshold", Color.white));
-        inputs.add(new GUICommandField("reproThreshField", this, nextPlace(parent.getStartPos(), 4, -4).add(BUTTON_SIZE), (BUTTON_SIZE.x), Color.black));
-        components.add(new GUILabel("hunterThreshLabel", nextPlace(parent.getStartPos(), 3, -2), BUTTON_SIZE, "Hunt Threshold", Color.white));
-        inputs.add(new GUICommandField("hunterThreshField", this, nextPlace(parent.getStartPos(), 4, -3).add(BUTTON_SIZE), (BUTTON_SIZE.x), Color.black));
+        components.add(new GUILabel("reproThreshLabel", nextPlace(parent.getStartPos(), 3, -2), BUTTON_SIZE, "Mate Threshold", Color.white));
+
+        inputs.add(new GUIButton("rtfb", this, nextPlace(parent.getStartPos(), 4, -2), BUTTON_SIZE, "", Color.white));
+        inputs.add(new GUICommandField("rtf", this, nextPlace(parent.getStartPos(), 4, -2).add(new Vec2(6, (BUTTON_SIZE.y - FONT.getHeight()) / 2.0 + FONT.getHeight())), BUTTON_SIZE.x - 12, Color.white, Color4.WHITE));
         //RIP THRESHOLDS
         //Other
         components.add(new GUILabel("reproBufferLabel", nextPlace(parent.getStartPos(), 3, -1), BUTTON_SIZE, "Mating Buffer", Color.white));
-        inputs.add(new GUICommandField("reproBufferField", this, nextPlace(parent.getStartPos(), 4, -2).add(BUTTON_SIZE), BUTTON_SIZE.x, Color.black));
-        components.add(new GUILabel("actionRadiusLabel", nextPlace(parent.getStartPos(), 3, -0), BUTTON_SIZE, "Action Radius", Color.white));
-        inputs.add(new GUICommandField("actionRadiusField", this, nextPlace(parent.getStartPos(), 4, -1).add(BUTTON_SIZE), BUTTON_SIZE.x, Color.black));
+
+        inputs.add(new GUIButton("rbfb", this, nextPlace(parent.getStartPos(), 4, -1), BUTTON_SIZE, "", Color.white));
+        inputs.add(new GUICommandField("rbf", this, nextPlace(parent.getStartPos(), 4, -1).add(new Vec2(6, (BUTTON_SIZE.y - FONT.getHeight()) / 2.0 + FONT.getHeight())), BUTTON_SIZE.x - 12, Color.white, Color4.WHITE));
+
+        components.add(new GUILabel("actionRadiusLabel", nextPlace(parent.getStartPos(), 3, 0), BUTTON_SIZE, "Action Radius", Color.white));
+
+        inputs.add(new GUIButton("arfb", this, nextPlace(parent.getStartPos(), 4, 0), BUTTON_SIZE, "", Color.white));
+        inputs.add(new GUICommandField("arf", this, nextPlace(parent.getStartPos(), 4, 0).add(new Vec2(6, (BUTTON_SIZE.y - FONT.getHeight()) / 2.0 + FONT.getHeight())), BUTTON_SIZE.x - 12, Color.white, Color4.WHITE));
 
 //                 inputs for:
 //                 <Creature>
@@ -71,7 +79,6 @@ public class CreaturePresetSpec extends ComponentInputGUI {
 //                 eCostMove- COMPLETE
 //                 <Terrain>
 //                 actionRadius- COMPLETE
-
         System.out.println("all creature components constructed according to plan");
     }
 
@@ -87,35 +94,83 @@ public class CreaturePresetSpec extends ComponentInputGUI {
         int newValInt;
         switch (string) {
             case "cancelBtn":
+
+                ((GUICommandField) inputs.get(getFieldIndex("rtf"))).send();
+                ((GUICommandField) inputs.get(getFieldIndex("htf"))).send();
+                ((GUICommandField) inputs.get(getFieldIndex("rbf"))).send();
+                ((GUICommandField) inputs.get(getFieldIndex("arf"))).send();
+                
                 setVisible(false);
                 parent.start();
                 break;
+
             case "eCostSetVisible":
+
                 eCostMenu.start();
                 break;
-            case "reproThreshField":
+
+            case "rtf":
+
                 inputStr = (String) o;
-                newValDouble = parseDouble(inputStr);
-                NewPreset.reproThreshold = newValDouble;
-                ((GUICommandField) inputs.get(getFieldIndex("reproThreshField"))).setText(inputStr);
+
+                try {
+
+                    newValDouble = parseDouble(inputStr);
+                    NewPreset.reproThreshold = newValDouble;
+                    ((GUICommandField) inputs.get(getFieldIndex("rtf"))).setText(inputStr);
+                } catch (Exception e) {
+
+                    ((GUICommandField) inputs.get(getFieldIndex("rtf"))).setText("Error");
+                }
+
                 break;
-            case "hunterThreshField":
+
+            case "htf":
+
                 inputStr = (String) o;
-                newValDouble = parseDouble(inputStr);
-                NewPreset.hunterThreshold = newValDouble;
-                ((GUICommandField) inputs.get(getFieldIndex("hunterThreshField"))).setText(inputStr);
+
+                try {
+
+                    newValDouble = parseDouble(inputStr);
+                    NewPreset.hunterThreshold = newValDouble;
+                    ((GUICommandField) inputs.get(getFieldIndex("htf"))).setText(inputStr);
+                } catch (Exception e) {
+
+                    ((GUICommandField) inputs.get(getFieldIndex("htf"))).setText("Error");
+                }
+
                 break;
-            case "reproBufferField":
+
+            case "rbf":
+
                 inputStr = (String) o;
-                newValDouble = parseDouble(inputStr);
-                NewPreset.reproBuffer = newValDouble;
-                ((GUICommandField) inputs.get(getFieldIndex("reproBufferField"))).setText(inputStr);
+
+                try {
+
+                    newValDouble = parseDouble(inputStr);
+                    NewPreset.reproBuffer = newValDouble;
+                    ((GUICommandField) inputs.get(getFieldIndex("rbf"))).setText(inputStr);
+                } catch (Exception e) {
+
+                    ((GUICommandField) inputs.get(getFieldIndex("rbf"))).setText("Error");
+                }
+
                 break;
-            case "actionRadiusField":
+
+            case "arf":
+
                 inputStr = (String) o;
-                newValInt = parseInt(inputStr);
-                NewPreset.actionRad = newValInt;
-                ((GUICommandField) inputs.get(getFieldIndex("actionRadiusField"))).setText(inputStr);
+
+                try {
+
+                    newValInt = parseInt(inputStr);
+                    NewPreset.actionRad = newValInt;
+                    ((GUICommandField) inputs.get(getFieldIndex("arf"))).setText(inputStr);
+                } catch (Exception e) {
+
+                    ((GUICommandField) inputs.get(getFieldIndex("arf"))).setText("Error");
+                }
+
                 break;
 
         }
@@ -132,6 +187,23 @@ public class CreaturePresetSpec extends ComponentInputGUI {
             }
         }
         return -1;
+    }
+
+    @Override
+    public List<GUIComponent> mousePressed(Vec2 p) {
+
+        List<GUIComponent> lgc = super.mousePressed(p);
+
+        for (int i = 0; i < 3; i++) {
+
+            if (lgc.contains(inputs.get(i * 2 + 2))) {
+
+                lgc.remove(inputs.get(i * 2 + 2));
+                lgc.add(inputs.get(i * 2 + 3));
+            }
+        }
+
+        return lgc;
     }
 
     @Override
